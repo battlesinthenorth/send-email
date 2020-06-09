86324ba4faa999a68974df29f76ffa7aa275d2dc
Eduardo Franco <battlesinthenorth@gmail.com>
2020-06-09 17:08:04 -0600

    Update send.sh

--- send.sh
+++ send.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-pwd > /home/interfaz/ReporteInvSidra/logsend.log
+
 BOUNDARY=$(date +%s|md5sum)
 BOUNDARY=${BOUNDARY:0:32}
 DATE=`date +%d%m%Y`
 filepath="/home/interfaz/ReporteInvSidra/REPORTE_INVENTARIO_SIDRA.csv"
-filename=$(basename ${filepath})
-
+filename=$(basename ${filepath}) 
 
 CONTENT=$(cat <<-END
 From: reportes-sidra@millicom.com
@@ -13,54 +12,30 @@ To: ivan.alvarado@tigo.com.pa, rosmeryuleima.rojasluna.mail@tigo.com.pa, ben.sol
 Reply-To: produccion.comercial.sidra.ca@telefonica.com
 Subject: Reporte Inventario SIDRA ${DATE}
 Content-Type: multipart/mixed; boundary="${BOUNDARY}"
-This is a MIME formatted message. If you see this text it means that your
-email software does not support MIME formatted messages, but as plain text
-encoded you should be ok, with a plain text file.
---${BOUNDARY}
-Content-Type: text/html; charset=ISO-8859-1; format=flowed
-Content-Transfer-Encoding: 8bit
-Content-Disposition: inline
-Buenos dias, se adjunta reporte diario de inventarios SIDRA automatizado. Para la correcta visualizacion del archivo, exportar de filas a columnas en Excel la columna A seleccionando el caracter "|" . Saludos.
---${BOUNDARY}
-Content-Type: text/csv; name="${filename}"
-Content-Transfer-Encoding: 8bit
-Content-Disposition: attachment; filename="${filename}"
-$(cat ${filename})
---${BOUNDARY}--
-END
-)
-
-
-echo "$CONTENT" | /usr/sbin/sendmail -t 2>/dev/null
-
-Reply-To: produccion.comercial.sidra.ca@telefonica.com
-Subject: Reporte Inventario SIDRA ${DATE}
-Content-Type: multipart/mixed; boundary="${BOUNDARY}"
 
 This is a MIME formatted message. If you see this text it means that your
 email software does not support MIME formatted messages, but as plain text
 encoded you should be ok, with a plain text file.
 
---${BOUNDARY}
+--${BOUNDARY} 
 Content-Type: text/html; charset=ISO-8859-1; format=flowed
 Content-Transfer-Encoding: 8bit
 Content-Disposition: inline
 
-Buenos dias, se adjunta reporte diario de inventarios SIDRA automatizado. Para la correcta visualizacion del archivo, exportar de filas a columnas en Excel la columna A seleccionando el caracter | . Saludos.
+Buenos dias, se adjunta reporte diario de inventarios SIDRA automatizado. Para la correcta visualizacion del archivo, favor de seleccionar la opcion de exportar de filas a columnas en Excel, seleccionar toda la columna A y colocar el caracter "|" . Saludos.
 
 --${BOUNDARY}
 Content-Type: text/csv; name="${filename}"
-Content-Transfer-Encoding: 8bit
+Content-Transfer-Encoding: 8bit 
 Content-Disposition: attachment; filename="${filename}"
 
 $(cat ${filepath})
 
---${BOUNDARY}--
+--${BOUNDARY}-- 
 END
 )
 
 
 echo "$CONTENT" | /usr/sbin/sendmail -t 2>/dev/null
-exit
 
 
